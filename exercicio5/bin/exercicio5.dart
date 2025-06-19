f4a4151232e93098d3d7730569e517683f7431bf
Nicolas Fenato <nicfenato@gmail.com>
2025-06-19 01:02:20 -0300

    Exercício 5

---
+++ exercicio5/bin/exercicio5.dart
@@ -0,0 +1,9 @@
+void main() {
+  String valorA = "30";
+  String valorB = "15.75";
+  
+  var soma = double.parse(valorA) + double.parse(valorB);
+  
+  print("A soma de $valorA e $valorB é: $soma");
+
+}
